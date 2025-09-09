4a97d3981dc40475b0a66c064ecf55c9f8bb56b3
Yun-choice <choice019@naver.com>
2025-09-09 15:44:19 +0900

    Quartz sync: Sep 9, 2025, 3:44 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -27,7 +27,7 @@ export default (() => {
     const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
     const iconPath = joinSegments(baseDir, "static/icon.png")
 
-    // Url of current page
+    // Url of current page, SEO(google search console)연동
     const socialUrl =
       fileData.slug === "404" ? url.toString() : joinSegments(url.toString(), fileData.slug!)
 
@@ -52,7 +52,7 @@ export default (() => {
         )}
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
+        <meta name="google-site-verification" content="R9VYtSkmxZNYUKAY0Jy7qji3HY3ZNPLdR9N7v4Tg5kE" />
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
         <meta property="og:title" content={title} />
         <meta property="og:type" content="website" />
