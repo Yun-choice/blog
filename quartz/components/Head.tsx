01a9c7cd0f2126689bc111284a061615e6fd55c9
taketech019 <taketech019@gmail.com>
2025-11-28 04:18:00 +0900

    seo-tag-update-head.ts: 2025-11-28 04:18:00

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -52,7 +52,7 @@ export default (() => {
         )}
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="google-site-verification" content="R9VYtSkmxZNYUKAY0Jy7qji3HY3ZNPLdR9N7v4Tg5kE" />
+        <meta name="google-site-verification" content="Niqn1oj5i-2-MVNqa9tk7kDtNsics4vEOWtdTzDqzuE" />
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
         <meta property="og:title" content={title} />
         <meta property="og:type" content="website" />
